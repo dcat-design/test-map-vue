7e922daad1bd35853723cf803b46d99fe2669a8b
Vadim Lihachev <dcat.design@gmail.com>
2019-07-02 23:43:12 +0400

    3 map points in row

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Map :center="center" :points="points"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    Map,
+  },
+  data: () => ({
+    center: [56.324285, 43.954825],
+    points: [
+      {
+        id: '1',
+        address:'Адрес номер 1',
+        lat:'56.338995',
+        lon:'43.909850',
+      },
+      {
+        id: '2',
+        address:'Адрес номер 2',
+        lat:'56.328195',
+        lon:'43.961298',
+      },
+      {
+        id: '3',
+        address:'Адрес номер 3',
+        lat:'56.335088',
+        lon:'43.937985',
+      },
+    ],
+  })
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,67 @@
+<template>
+  <yandex-map
+    class="Map"
+    :settings="settings"
+    :coords="center"
+    zoom="12"
+  >
+    <ymap-marker
+      v-for="point in pointsList"
+      :key="point.id"
+      :marker-id="point.id"
+      :coords="point.coords"
+      :hint-content="point.comment"
+      marker-type="Placemark"
+    />
+  </yandex-map>
+</template>
+
+<script>
+import { yandexMap, ymapMarker } from 'vue-yandex-maps'
+
+export default {
+  components: {
+    yandexMap,
+    ymapMarker
+  },
+  props: {
+    points: Array,
+    center: {
+      type: Array,
+      required: true,
+    }
+  },
+  data: () => ({
+    settings: {
+      apiKey: '292a1fb2-2d7b-4735-a4e9-d42c7db4f9e3',
+      lang: 'ru_RU',
+      coordorder: 'latlong',
+      version: '2.1',
+    },
+  }),
+  computed: {
+    pointsList() {
+      return this.points.reduce((arr, el) => {
+        const {id, address, lat, lon} = el;
+        const comment = `
+          Task name: <strong>${id}</strong><br/>
+          Address: <strong>${address}</strong>
+        `;
+        const point = {
+          id,
+          comment,
+          coords: [ lat, lon ],
+        };
+        return [...arr, point];
+      }, []);
+    }
+  },
+}
+</script>
+
+<style>
+.Map {
+  width: calc(100vw - 40px);
+  height: calc(100vh - 40px);
+}
+</style>
